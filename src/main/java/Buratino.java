647638f44cc92bd8fd7b0239bcb641486d7478e8
kukuwwka <ququwka1@gmail.com>
2023-10-30 09:55:13 +0400

    inital commit

--- src/main/java/Buratino.java
+++ src/main/java/Buratino.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Buratino {
+public class Buratino {
+    public static void main(String[] args) {
+        final int AppBuratino = 214;
+        final int AppKarabas = 123;
+        final int KarabasVor = AppBuratino - AppKarabas;
+        System.out.println(KarabasVor);
+    }
 }
