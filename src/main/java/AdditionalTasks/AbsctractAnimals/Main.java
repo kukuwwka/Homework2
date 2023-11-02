0095fcd343a7441b9c34b6b72ab5bea25163fb84
kukuwwka <ququwka1@gmail.com>
2023-11-02 13:32:24 +0400

    initial commit

--- src/main/java/AdditionalTasks/AbsctractAnimals/Main.java
+++ src/main/java/AdditionalTasks/AbsctractAnimals/Main.java
@@ -1,10 +1,5 @@
 package AdditionalTasks.AbsctractAnimals;
 
-import AdditionalTasks.AbsctractAnimals.Animal;
-import AdditionalTasks.AbsctractAnimals.Cat;
-import AdditionalTasks.AbsctractAnimals.Duck;
-import AdditionalTasks.AbsctractAnimals.Fish;
-
 public class Main {
     public static void main(String[] args) {
         Cat cat1 = new Cat();
@@ -15,6 +10,5 @@ public class Main {
 
         Fish fish1 = new Fish();
         fish1.move();
-
     }
 }
